187552ce43106a8dd0a8277abfa5b5727638b5f5
salimcobanoglu <salimcobanoglu@yahoo.com>
2023-06-04 13:18:46 +0300

    auth-model get&create added

---
+++ api/auth/auth-model.js
@@ -0,0 +1,25 @@
+const db = require("../../data/dbconfig")
+
+function getAll (){
+return db("users");
+}
+
+function getById (id) {
+return db("users").where("id", id).first();
+}
+
+function getBy(filter){
+return db("users").where(filter).first();
+}
+
+async function create (user){
+    const insertedUser = await db("users").insert(user);
+return getById(insertedUser[0]);
+}
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    getBy
+}
