c262115b08899eddfb2ffed003f9f4ea31efe689
salimcobanoglu <salimcobanoglu@yahoo.com>
2023-06-04 14:41:27 +0300

    Migration up & down added, db created

--- data/migrations/20230603091020_migrationfile.js
+++ data/migrations/20230603091020_migrationfile.js
@@ -3,7 +3,13 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("users", function(table){
+table.increments("user_id");
+table.string("username").notNullable().unique();
+table.string("password").notNullable();
+table.string("avatar_url").notNullable();
+table.string("email").notNullable().unique();
+  });
 };
 
 /**
@@ -11,5 +17,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("users");
 };
