673e9d83845bd106eca5b6fa4febbe908b95bdb3
salimcobanoglu <salimcobanoglu@yahoo.com>
2023-06-07 12:57:56 +0300

    post model & mw revised

---
+++ api/posts/post-router.js
@@ -0,0 +1,87 @@
+const router = require("express").Router();
+const postModel = require("./post-model");
+const mw = require("./post-middleware");
+// const favsMw = require("../favorites/favorites-middleware");
+
+// Gel all posts
+router.get("/", async (req, res, next) => {
+  try {
+    const posts = await postModel.getAll();
+    res.status(200).json(posts);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//Create new post
+router.post("/", mw.checkPayload, async (req, res, next) => {
+  try {
+    const { body, image_url, user_id } = req.body;
+    const newPost = { user_id: user_id, body: body, image_url: image_url };
+    const insertedPost = await postModel.create(newPost);
+    if (!insertedPost) {
+      next(error);
+    } else {
+      res
+        .status(200)
+        .json({ message: "New post successfully submitted.", insertedPost });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+//Update post
+
+router.put("/:id", mw.checkPayload, async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const { body, image_url, user_id } = req.body;
+    const newPost = {
+      user_id: user_id,
+      body: body,
+      image_url: image_url,
+    };
+    const updatedPost = await postModel.update(id, newPost);
+    if (!updatedPost) {
+      next(error);
+    } else {
+      res
+        .status(200)
+        .json({ message: "Edited post successfully submitted.", updatedPost });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+//Delete post
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const deletedPost = await postModel.remove(id);
+    if (!deletedPost) {
+      res.status(400).json({ message: `Post with id: ${id} is not found.` });
+    } else {
+      res.status(200).json({ message: "Post removed successfully." });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+//Get favorite post
+// router.get(
+//   "/:id/favorites",
+//   favsMw.checkFavsByPostId,
+//   async (req, res, next) => {
+//     try {
+//       res.status(200).json(req.favUsers);
+//     } catch (error) {
+//       next(error);
+//     }
+//   }
+// );
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const server = express();
 const authRouter = require("../api/auth/auth-router");
 const usersRouter = require("./users/users-router");
+const postsRouter = require("../api/posts/post-router");
 
 server.use(express.json());
 server.use("/api/auth", authRouter);
 server.use("/api/users", usersRouter);
+server.use("/api/posts", postsRouter);
 
 server.get("/", (reg, res) => {
   res.json({ message: "welcome to my app!..." });
